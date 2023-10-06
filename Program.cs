08c82d5a920c5f80800bcfe2879d85a7a0d27aae
Renat Aubakirov <arenat98@mail.ru>
2023-10-06 12:17:32 +0700

    с кодом

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 29 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+5 -> [1, 2, 5, 7, 19]; 3 -> [6, 1, 33] */
+
+int lenArray = 8;
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
